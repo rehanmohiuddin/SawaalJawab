e715cf1c7df6b0e94bb71f556847e5aa07b8dbab
rehanmohiuddin <rehan.4942@gmail.com>
2022-05-01 15:27:52 +0530

    Feat:Header Responsive

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./index.scss";
 import Logo from "../../assets/SawaalJawab -mailer.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
+  const [showMobNav, setMobNav] = useState(null);
   const options = [
     {
       name: "Home",
@@ -18,20 +21,34 @@ function Header() {
       action: null,
     },
   ];
+
+  const RoutesRender = ({ _className }) => (
+    <ul className={_className}>
+      {options.map((option) =>
+        option.route ? (
+          <Link to={`${option.route}`}>{option.name}</Link>
+        ) : (
+          <li onClick={option.action}>{option.name}</li>
+        )
+      )}
+    </ul>
+  );
   return (
     <header>
-      <Link to={"/"}>
-        <img className="logo" src={Logo} />
-      </Link>
-      <ul>
-        {options.map((option) =>
-          option.route ? (
-            <Link to={`${option.route}`}>{option.name}</Link>
-          ) : (
-            <li onClick={option.action}>{option.name}</li>
-          )
-        )}
-      </ul>
+      <div>
+        <Link to={"/"}>
+          <img className="logo" src={Logo} />
+        </Link>
+        <RoutesRender _className={"desktop-container"} />
+        <FontAwesomeIcon
+          className="mobNav"
+          onClick={() => setMobNav(!showMobNav)}
+          icon={faBars}
+          size="2x"
+        />
+      </div>
+
+      {showMobNav && <RoutesRender _className={"mobNav-container"} />}
     </header>
   );
 }
